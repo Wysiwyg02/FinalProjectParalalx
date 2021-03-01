6c8acf94412d0de36cfa902ca151782820c88bdc
NatureMorning <kevchristophermorco@gmail.com>
2021-03-01 22:36:35 +0800

    Initial commit

---
+++ parallax/src/main/java/com/naturemorning/parallax/views/FxmlView.java
@@ -0,0 +1,72 @@
+package com.naturemorning.parallax.views;
+
+import java.util.ResourceBundle;
+
+/**
+ * OOP Class 20-21
+ *
+ * @author Gerald Villaran
+ */
+public enum FxmlView {
+
+    USER {
+        @Override
+        public String getTitle() {
+            return getStringFromResourceBundle("user.title");
+        }
+
+        @Override
+        public String getFxmlFile() {
+            return "/fxml/User.fxml";
+        }
+    },LOGIN {
+        @Override
+        public String getTitle() {
+            return getStringFromResourceBundle("login.title");
+        }
+
+        @Override
+        public String getFxmlFile() {
+            return "/fxml/LogIn.fxml";
+        }
+    },CASHIER {
+        @Override
+        public String getTitle() {
+            return getStringFromResourceBundle("cashier.title");
+        }
+
+        @Override
+        public String getFxmlFile() {
+            return "/fxml/Cashier.fxml";
+        }
+    },CASHIERDELIVERY {
+        @Override
+        public String getTitle() {
+            return getStringFromResourceBundle("cashierdelivery.title");
+        }
+
+        @Override
+        public String getFxmlFile() {
+            return "/fxml/CashierMenuDelivery.fxml";
+        }
+    },CASHIERRESERVATION {
+        @Override
+        public String getTitle() {
+            return getStringFromResourceBundle("cashierreservation.title");
+        }
+
+        @Override
+        public String getFxmlFile() {
+            return "/fxml/CashierReservation.fxml";
+        }
+    };
+
+    public abstract String getTitle();
+
+    public abstract String getFxmlFile();
+
+    String getStringFromResourceBundle(String key) {
+        return ResourceBundle.getBundle("Bundle").getString(key);
+    }
+
+}
